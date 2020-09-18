7f1f2a655f27e94dcda7ad88e0723aa0191677dd
Jia-QuanCheng <Jia-QuanCheng@0-208-02>
2020-09-18 12:52:41 +0200

    Good morning

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,18 @@
 
 public class HelloWorld {
+	
+	static int counter = 0;
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		
+		while(counter<100) {
+			if (counter%2==0) {System.out.println("Ohio Sekai");}
+			else {System.out.println("Good morning World");}
+		System.out.println(counter);
+		counter++;
+		
+		}
+	
 	}
 
 }
