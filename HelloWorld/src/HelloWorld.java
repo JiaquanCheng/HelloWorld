576066d920ad75f51dede8386824c5392a72107a
Jia-QuanCheng <Jia-QuanCheng@0-208-02>
2020-09-25 13:00:28 +0200

    Prime

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,19 +1,22 @@
 
 public class HelloWorld {
 	
-	static int number = 13;
 	static int counter = 2;
 	static boolean isPrime = true;
 	
 	public static void main(String[] args) {
-		
-		while(counter<number/2 && isPrime==true) {
-			if(number%counter==0) {isPrime=false;}
 			
-			counter++;
-		
+		for(int number=2;number<1000;number++) {
+			
+			while(counter<=number/2 && isPrime==true) {
+				if(number%counter==0) {isPrime=false;}
+				
+				counter++;
+			
+			}
+			counter=2;
+		if(isPrime==true) {System.out.println(number);}
+		else {isPrime=true;}
 		}
-	if(isPrime==true) {System.out.println(number);}
 	}
-
 }
